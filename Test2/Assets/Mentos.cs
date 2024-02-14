a33c1e750452c424d6ea4ce6c39863f4a9a10c6c
Kim-game3 <zweilous387@icloud.com>
2024-02-14 20:11:29 +0900

    更新ing

---
+++ Test2/Assets/Mentos.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mentos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Oyasuminasai");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
